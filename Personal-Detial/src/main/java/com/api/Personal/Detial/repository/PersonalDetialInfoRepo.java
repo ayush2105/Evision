1bb0dc28997f0365eea442fd1487b1fd21afd140
ayush2105 <ayush.parmar7879@gmail.com>
2024-02-20 12:49:01 +0530

    Added

---
+++ Personal-Detial/src/main/java/com/api/Personal/Detial/repository/PersonalDetialInfoRepo.java
@@ -0,0 +1,7 @@
+package com.api.Personal.Detial.repository;
+
+import com.api.Personal.Detial.entities.PersonalDetailInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonalDetialInfoRepo extends JpaRepository<PersonalDetailInfo,Integer> {
+}
---
+++ Personal-Detial/src/test/java/com/api/Personal/Detial/PersonalDetialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Personal.Detial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDetialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
